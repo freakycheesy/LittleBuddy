5a44b32f3c2228f5a596d2b7dde3c3790006e655
CarrionAndOn <maya@carrionandon.me>
2023-08-20 16:34:10 -0700

    Changed spawn method

--- CloneRigManager.cs
+++ CloneRigManager.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+﻿using BoneLib.Nullables;
+using BoneLib;
+using UnityEngine;
+using SLZ.Marrow.Warehouse;
+using SLZ.Marrow.Pool;
+using SLZ.Marrow.Data;
+using System;
+using SLZ.VRMK;
+
 public class CloneRigManager
 {
     public string objectToDuplicateName = "[RigManager (Blank)]";
@@ -6,50 +14,81 @@ public class CloneRigManager
 
     public void Delete()
     {
-        GameObject buddyObject = GameObject.Find("LittleBuddy");
-        Object.Destroy(buddyObject);
+        GameObject buddyObject = GameObject.Find("Default Player Rig (0)");
+        UnityEngine.Object.Destroy(buddyObject);
     }
     public void Clone()
     {
-        // Find the rig manager
-        GameObject originalObject = GameObject.Find(objectToDuplicateName);
+        Transform head = Player.playerHead.transform;
 
-        if (originalObject != null)
+        string barcode = "SLZ.BONELAB.Core.DefaultPlayerRig";
+        SpawnableCrateReference reference = new SpawnableCrateReference(barcode);
+
+        Spawnable spawnable = new Spawnable()
         {
-            // Duplicate the rig manager
-            GameObject duplicatedObject = Object.Instantiate(originalObject);
+            crateRef = reference
+        };
 
-            // Offset the rigmanager clone by 5 on X axis
-            duplicatedObject.transform.position += offset;
-
-            // Remove UI related objects from the rig manager
-            Transform spectatorCamera = duplicatedObject.transform.Find("Spectator Camera");
-            if (spectatorCamera != null)
-            {
-                Object.Destroy(spectatorCamera.gameObject);
-            }
-
-            Transform uiRig = duplicatedObject.transform.Find("[UIRig]");
-            if (uiRig != null)
-            {
-                Object.Destroy(uiRig.gameObject);
-            }
-
-            Transform spawnGunUI = duplicatedObject.transform.Find("SpawnGunUI");
-            if (spawnGunUI != null)
-            {
-                Object.Destroy(spawnGunUI.gameObject);
-            }
-
-            Transform twoDOverlay = duplicatedObject.transform.Find("2D_Overlay");
-            if (twoDOverlay != null)
-            {
-                Object.Destroy(twoDOverlay.gameObject);
-            }
-        }
-        else
+        AssetSpawner.Register(spawnable);
+        Action<GameObject> spawnAction = go =>
         {
-            Debug.LogWarning("Object to duplicate not found: " + objectToDuplicateName);
-        }
+            Transform rigManager = go.transform.Find("[Rig Manager (Blank)]");
+            if (rigManager != null) 
+            {
+                Avatar[] avatarComponents = rigManager.GetComponentsInChildren<Avatar>();
+                foreach (Avatar avatar in avatarComponents)
+                {
+                    foreach (var mesh in avatar.hairMeshes)
+                    {
+                        mesh.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided;
+                    }
+                }
+                Transform uiRig = rigManager.transform.Find("[UIRig]");
+                if (uiRig != null)
+                {
+                    UnityEngine.Object.Destroy(uiRig.gameObject);
+                }
+                Transform spectatorCamera = rigManager.transform.Find("Spectator Camera");
+                if (spectatorCamera != null)
+                {
+                    UnityEngine.Object.Destroy(spectatorCamera.gameObject);
+                }
+                Transform spawnGunUI = rigManager.transform.Find("SpawnGunUI");
+                if (spawnGunUI != null)
+                {
+                    UnityEngine.Object.Destroy(spawnGunUI.gameObject);
+                }
+                Transform eventSystem = rigManager.transform.Find("EventSystem");
+                if (eventSystem != null)
+                {
+                    UnityEngine.Object.Destroy(eventSystem.gameObject);
+                }
+                Transform twoDOverlay = rigManager.transform.Find("2D_Overlay");
+                if (twoDOverlay != null)
+                {
+                    UnityEngine.Object.Destroy(twoDOverlay.gameObject);
+                }
+                Transform ocr = rigManager.transform.Find("[OpenControllerRig]");
+                {
+                    if (ocr != null)
+                    {
+                        Transform trackingSpace = ocr.transform.Find("TrackingSpace");
+                        if (trackingSpace != null)
+                        {
+                            Transform headobj = trackingSpace.Find("Head");
+                            if (head != null)
+                            {
+                                Camera cameraComponent = headobj.GetComponent<Camera>();
+                                if (cameraComponent != null)
+                                {
+                                    cameraComponent.enabled = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        AssetSpawner.Spawn(spawnable, head.position + head.forward, default, new BoxedNullable<Vector3>(Vector3.one), false, new BoxedNullable<int>(null), null, null);
     }
 }
